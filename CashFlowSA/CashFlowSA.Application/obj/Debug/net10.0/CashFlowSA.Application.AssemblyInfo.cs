1ed6f49fe38f13a420ce1efc8ab182143e257092
Laeeqah Ryklief <23muneebryklief@gmail.com>
2026-07-13 15:51:32 +0200

    added db context and configuration classes to simplify the db context

--- CashFlowSA/CashFlowSA.Application/obj/Debug/net10.0/CashFlowSA.Application.AssemblyInfo.cs
+++ CashFlowSA/CashFlowSA.Application/obj/Debug/net10.0/CashFlowSA.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlowSA.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22d18891f7f88c1c41a884d3944c51d8684ddeb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7214a81046ceda1eec57134a8c5a9553ca3b60b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlowSA.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlowSA.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

a7214a81046ceda1eec57134a8c5a9553ca3b60b
Laeeqah Ryklief <23muneebryklief@gmail.com>
2026-07-13 15:12:09 +0200

    fixed namespaces of model to align with the new architecture style

--- CashFlowSA/CashFlowSA.Application/obj/Debug/net10.0/CashFlowSA.Application.AssemblyInfo.cs
+++ CashFlowSA/CashFlowSA.Application/obj/Debug/net10.0/CashFlowSA.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlowSA.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec2d4d016e0df571229858423d8bbe37ca6dbc97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22d18891f7f88c1c41a884d3944c51d8684ddeb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlowSA.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlowSA.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

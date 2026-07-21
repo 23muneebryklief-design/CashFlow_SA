c067b866316280d5a05d3d7e8c1a69d8541ab271
Laeeqah Ryklief <23muneebryklief@gmail.com>
2026-07-21 09:45:13 +0200

    added marketplace controller

--- CashFlowSA/CashFlowSA.Application/obj/Debug/net10.0/CashFlowSA.Application.AssemblyInfo.cs
+++ CashFlowSA/CashFlowSA.Application/obj/Debug/net10.0/CashFlowSA.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlowSA.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61ac4e357d750ab08565dbd30db2617d58baa28d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a0de5baaefca950d1704cebb68905b33662fb48")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlowSA.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlowSA.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

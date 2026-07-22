01d34295a1f17c0525c7023959e771534e0fae96
Laeeqah Ryklief <23muneebryklief@gmail.com>
2026-07-22 14:31:53 +0200

    comepleted controllers

--- CashFlowSA/CashFlowSA.API/obj/Debug/net10.0/CashFlowSA.API.AssemblyInfo.cs
+++ CashFlowSA/CashFlowSA.API/obj/Debug/net10.0/CashFlowSA.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlowSA.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0982044e95cf4bc21baea459dbea1bcd6f0d5afc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a51ab4e021f16dc137f3eb48fb73acac4134d2cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlowSA.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlowSA.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
